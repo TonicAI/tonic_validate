7781d67ab030166dc1a3cf70ddf6fe47afea9204
ethan-tonic <ephilpott@tonic.ai>
2024-01-31 15:30:25 -0500

    Clean up benchmark

--- tonic_validate/classes/benchmark.py
+++ tonic_validate/classes/benchmark.py
@@ -1,34 +1,17 @@
-from typing import Iterator, List, Optional
-from dataclasses import dataclass
-from uuid import UUID
+from typing import Iterator, List, Optional, TypedDict
+
+from attr import dataclass
+
+
+class BenchmarkItem(TypedDict):
+    question: str
+    answer: Optional[str]
 
 
 @dataclass
-class BenchmarkItem:
-    question: str
-    answer: Optional[str] = None
-    benchmark_id: Optional[UUID] = None
-
-
 class Benchmark:
-    def __init__(
-        self,
-        questions: List[str],
-        answers: Optional[List[str]] = None,
-        name: Optional[str] = None,
-    ):
-        self.name: Optional[str] = name
-        self.items: List[BenchmarkItem] = []
-
-        if answers is None:
-            for question in questions:
-                self.items.append(BenchmarkItem(question))
-            return
-
-        if len(questions) != len(answers):
-            raise ValueError("Questions and answers must be the same length")
-        for question, answer in zip(questions, answers):
-            self.items.append(BenchmarkItem(question, answer))
+    name: Optional[str]
+    items: List[BenchmarkItem]
 
     # define iterator
     def __iter__(self) -> Iterator[BenchmarkItem]:

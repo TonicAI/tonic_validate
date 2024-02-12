d3a799ce7ba9fb931ab414fa6569b0c6a5279996
ethan-tonic <ephilpott@tonic.ai>
2024-02-12 18:58:24 -0500

    Fix List Typing

--- tonic_validate/classes/llm_response.py
+++ tonic_validate/classes/llm_response.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import TypedDict
+from typing import List, TypedDict
 
 from tonic_validate.classes.benchmark import BenchmarkItem
 
@@ -7,9 +7,10 @@ from tonic_validate.classes.benchmark import BenchmarkItem
 @dataclass
 class LLMResponse:
     llm_answer: str
-    llm_context_list: list[str]
+    llm_context_list: List[str]
     benchmark_item: BenchmarkItem
+
 
 class CallbackLLMResponse(TypedDict):
     llm_answer: str
-    llm_context_list: list[str]
+    llm_context_list: List[str]

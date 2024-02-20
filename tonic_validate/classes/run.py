c97a975c33484e86a152b78fa6333dbdc90d91a6
Adam Kamor <9391841+akamor@users.noreply.github.com>
2024-02-20 12:47:52 -0500

    Ability to create a Pandas DataFrame from a run's scores (#57)
    Create a dataset from the run.run_data dictionary.
    
    ---------
    
    Co-authored-by: ethan-tonic <ephilpott@tonic.ai>

--- tonic_validate/classes/run.py
+++ tonic_validate/classes/run.py
@@ -1,6 +1,9 @@
+import logging
 from typing import Any, List, Optional, Dict, Union
 from dataclasses import dataclass
 from uuid import UUID
+
+logger = logging.getLogger()
 
 
 @dataclass
@@ -26,3 +29,30 @@ class Run:
     overall_scores: Dict[str, float]
     run_data: List[RunData]
     id: Optional[UUID]
+
+    def to_df(self):
+        """
+        Convert the run data to a pandas DataFrame
+
+        Returns:
+            pd.DataFrame: DataFrame with the run data
+        """
+        try:
+            import pandas as pd
+        except ImportError as e:
+            logger.error(
+                "-------\n"
+                "Pandas not found. Please install to convert the run data to a dataframe.\n"
+                "-------"
+            )
+            raise e
+
+        metrics = list(self.overall_scores.keys())
+        columns = ["question"] + metrics
+        scores = []
+        for run in self.run_data:
+            run_score = [run.reference_question] + [
+                run.scores.get(metric, None) for metric in metrics
+            ]
+            scores.append(run_score)
+        return pd.DataFrame(scores, columns=columns)

0a9b0a66c892d0ddfb70b927d6b50b36c85e22c9
Lyon Van Voorhis <lyon@tonic.ai>
2024-03-07 12:28:33 -0500

    Revert "add sphinx"
    This reverts commit bdbe9fc0ecef99ed383ab8851f2a11639219e12b.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,13 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+from dataclasses import asdict
+from sphinxawesome_theme import ThemeOptions
+
+sys.path.insert(0, os.path.abspath("../.."))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -11,15 +18,40 @@ copyright = '2024, Tonic'
 author = 'Tonic'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
 
-templates_path = ['_templates']
+source_suffix = [".rst"]
+
+# Napoleon settings
+napoleon_google_docstring = False
+napoleon_use_param = False
+napoleon_use_ivar = True
+napoleon_include_init_with_doc = True
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = "sphinxawesome_theme"
+extensions += ["sphinxawesome_theme.highlighting"]
 
-html_theme = 'alabaster'
+html_title = "Tonic Validate Documentation"
+
+pygments_style = 'sphinx'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ["custom.css"]

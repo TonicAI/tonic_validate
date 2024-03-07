bdbe9fc0ecef99ed383ab8851f2a11639219e12b
Lyon Van Voorhis <lyon@tonic.ai>
2024-03-07 11:27:20 -0500

    add sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,13 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
-import sys
-from dataclasses import asdict
-from sphinxawesome_theme import ThemeOptions
-
-sys.path.insert(0, os.path.abspath("../.."))
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -18,40 +11,15 @@ copyright = '2024, Tonic'
 author = 'Tonic'
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-]
+extensions = []
 
-source_suffix = [".rst"]
-
-# Napoleon settings
-napoleon_google_docstring = False
-napoleon_use_param = False
-napoleon_use_ivar = True
-napoleon_include_init_with_doc = True
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = "sphinxawesome_theme"
-extensions += ["sphinxawesome_theme.highlighting"]
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_title = "Tonic Validate Documentation"
-
-pygments_style = 'sphinx'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = 'alabaster'
 html_static_path = ['_static']
-html_css_files = ["custom.css"]

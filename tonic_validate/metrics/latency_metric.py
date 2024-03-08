d45b9b8fb96c049b58f10bfcf46d28a1a983e882
ethan-tonic <ephilpott@tonic.ai>
2024-03-08 11:17:56 -0500

    Switch to binary metric

--- tonic_validate/metrics/latency_metric.py
+++ tonic_validate/metrics/latency_metric.py
@@ -1,17 +1,18 @@
 import logging
 from tonic_validate.classes.llm_response import LLMResponse
-from tonic_validate.metrics.metric import Metric
+from tonic_validate.metrics.binary_metric import BinaryMetric
 from tonic_validate.services.openai_service import OpenAIService
 
 logger = logging.getLogger()
 
 
-class LatencyMetric(Metric):
+class LatencyMetric(BinaryMetric):
     name: str = "latency_metric"
 
     def __init__(self, target_time: float = 5.0) -> None:
         """
-        Create a LatencyMetric.
+        Measures how long it takes for the LLM to complete a request.
+        Returns 0.0 if the LLM takes longer than the target time, and 1.0 otherwise.
 
         Parameters
         ----------
@@ -24,7 +25,6 @@ class LatencyMetric(Metric):
         # Check that llm_response.run_time is not None
         if llm_response.run_time is None:
             raise ValueError("No run time provided in LLMResponse")
-        # Find percentage of target time
-        if llm_response.run_time == 0:
-            return 1.0
-        return min(1.0, self.target_time / llm_response.run_time)
+        if llm_response.run_time > self.target_time:
+            return 0.0
+        return 1.0

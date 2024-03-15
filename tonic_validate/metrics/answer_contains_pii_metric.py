9db69b0e64cea07c30c511f7e39ef4c2bab42ff8
ethan-tonic <ephilpott@tonic.ai>
2024-03-15 16:47:48 -0400

    Fix import + clean up code

--- tonic_validate/metrics/answer_contains_pii_metric.py
+++ tonic_validate/metrics/answer_contains_pii_metric.py
@@ -4,7 +4,7 @@ from typing import List, Optional
 from tonic_validate.classes.llm_response import LLMResponse
 from tonic_validate.metrics.binary_metric import BinaryMetric
 from tonic_validate.services.openai_service import OpenAIService
-from tonic_textual.api import TonicTextual
+
 
 class AnswerContainsPiiMetric(BinaryMetric):
     def __init__(self, pii_types: List[str], textual_api_key: Optional[str] = None):
@@ -21,25 +21,39 @@ class AnswerContainsPiiMetric(BinaryMetric):
         Your Textual API key.  This value is optional. It is preferred that you set TONIC_TEXTUAL_API_KEY via your ENVIRONMENT.
 
         """
+        try:
+            from tonic_textual.api import TonicTextual
+        except ImportError:
+            raise ImportError(
+                "You must install tonic-textual to use the AnswerContainsPiiMetric. You can install it via pip: pip install tonic-textual"
+            )
         self.pii_types = [p.lower() for p in pii_types]
         if textual_api_key is None:
             if os.getenv("TONIC_TEXTUAL_API_KEY") is None:
-                raise ValueError("You must set TONIC_TEXTUAL_API_KEY in your ENV or pass your Textual API key into the constructor.")
+                raise ValueError(
+                    "You must set TONIC_TEXTUAL_API_KEY in your ENV or pass your Textual API key into the constructor."
+                )
             self.textual = TonicTextual("https://textual.tonic.ai")
         else:
             self.textual = TonicTextual("https://textual.tonic.ai", textual_api_key)
 
         super().__init__("answer_contains_pii", self.metric_callback)
 
-    def metric_callback(self, llm_response: LLMResponse, openai_service: OpenAIService) -> float:
+    def metric_callback(
+        self, llm_response: LLMResponse, openai_service: OpenAIService
+    ) -> float:
         try:
-            response = self.textual.redact('\n'.join(llm_response.llm_context_list))
+            response = self.textual.redact("\n".join(llm_response.llm_context_list))
             for d in response.de_identify_results:
                 if d.label.lower() in self.pii_types:
                     return 1.0
             return 0.0
         except requests.exceptions.HTTPError as e:
-             if e.response.status_code==401:
-                 raise ValueError('Cannot compute AnswerContainsPiiMetric. Your Textual API Key is INVALID.')
+            if e.response.status_code == 401:
+                raise ValueError(
+                    "Cannot compute AnswerContainsPiiMetric. Your Textual API Key is INVALID."
+                )
         except Exception:
-            raise ValueError('Cannot compute AnswerContainsPiiMetric. Error occured communicating with Textual.  Please try again later or reach out via GitHub issues.')
+            raise ValueError(
+                "Cannot compute AnswerContainsPiiMetric. Error occured communicating with Textual.  Please try again later or reach out via GitHub issues."
+            )
